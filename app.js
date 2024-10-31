80ab3bd518b9485d9996762bc7eddf5b9d4c7dc5
Arzoo Shamim <166494063+arzooshamim13@users.noreply.github.com>
2024-10-31 20:37:07 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+let main = document.getElementById("main");
+
+
+function btn(){
+    main.style.display = "block";
+}
+function hide(){
+    main.style.display = "none";
+}
+
+
+console.log(user)
+
+
+
+
